51729ee728a8a408b33db254adceaafd2a4eb46b
vinvintjg <vincent88tjong@gmail.com>
2023-11-19 19:54:21 +0700

    add: added Responsive tab MAIN PAGE

--- resources/views/user-home.blade.php
+++ resources/views/user-home.blade.php
@@ -532,9 +532,6 @@
                     <a href="/mangkas-faq">
                         <div class="foot-text-text">FAQ</div>
                     </a>
-                    {{-- <a href="/mangkas-chat">
-                        <div class="foot-text-text">LIVE CHAT</div>
-                    </a> --}}
                     <a href="/mangkas-contact">
                         <div class="foot-text-text">CONTACT</div>
                     </a>
